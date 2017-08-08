47818a2096b97508a1180217998dac21883e3683
mjf <mjf.php@gmail.com>
2017-08-08 19:55:27 +0100

    Operator table is ok

---
+++ database/migrations/2017_08_08_183246_create_operators_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOperatorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('operators', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('imm')->unique();
+            $table->string('name');
+            $table->string('n_pass');
+            $table->date('birthDate');
+            $table->string('ville');
+            $table->date('debutC');
+            $table->date('finC');
+            $table->string('examMedical');
+            $table->string('poste');
+            $table->string('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('operators');
+    }
+}
