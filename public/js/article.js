2af51ca1bc6933a909a00c005faef130340118e8
mjf <mjf.php@gmail.com>
2017-08-13 14:00:24 +0100

    Add an article

---
+++ public/js/article.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $('#table').DataTable();
+} );
+
+
+$(".delete-modal").on('click',function() {
+    $('#deleteA').modal('show');
+    var el = $(this).parent().prevAll();
+    $('#deleteA form').attr('action','/articles/' + el[8].innerText);
+
+});
+
+$(".edit-modal").on('click',function() {
+    $('#editA').modal('show');
+    var el = $(this).parent().prevAll();
+    $('#editA form').attr('action','/articles/' + el[8].innerText);
+    $('#editA #id').val(el[8].innerText);
+    $('#editA #ref').val(el[7].innerText);
+    $('#editA #name').val(el[6].innerText);
+    $('#editA #description').val(el[5].innerText);
+    $('#editA #standarPrice').val(el[4].innerText);
+    $('#editA #weightNet').val(el[3].innerText);
+    $('#editA #typ').val(el[2].innerText);
+    $('#editA #uomId').val(el[1].innerText);
+    $('#editA #state').val(el[0].innerText);
+});
+
+
+
+$(function () {
+    $('#modal').modal(toggle)
+});
---
+++ resources/views/article/add.blade.php
@@ -0,0 +1,132 @@
+@extends('layouts.app')
+
+@section('title', 'Ajouter Un Article')
+
+@section('content')
+
+
+    <div class="row">
+         <div class="col-md-4">
+        <h1>Add an article :</h1>
+         </div>
+         <div class="col-md-4"></div>
+        <a href="{{route('articles.index')}}" class="btn btn-primary col-md-2"><span class="glyphicon glyphicon glyphicon-arrow-left "> </span>   Retour</a>
+
+        </div>
+<br>
+<br>
+ <div class="container">
+
+
+      <form class="form-horizontal" role="form" method="post" action="/articles">
+        {{ csrf_field()}}
+   <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+
+
+      <div class="row">
+
+                       <div class="form-group">
+                          <label for="ref" class="col-md-3 control-label">Reference</label>
+                           <div class="col-md-4">
+                               <input type="text"  placeholder="reference" class="form-control" value="{{old('ref')}}" name="ref" autofocus required>
+                           </div>
+                        <div class="col-md-4"></div>
+                       </div>
+             </div>
+
+
+      <div class="row">
+
+                 <div class="form-group">
+                    <label for="name" class="col-md-3 control-label">Name</label>
+                     <div class="col-md-4">
+                         <input type="text"  placeholder="name" class="form-control" value="{{old('name')}}" name="name" autofocus required>
+                     </div>
+                  <div class="col-md-4"></div>
+                 </div>
+       </div>
+
+      <div class="row">
+                        <div class="form-group">
+                           <label for="description" class="col-md-3 control-label">Description</label>
+                            <div class="col-md-4">
+                                <input type="text"  placeholder="description" class="form-control" value="{{old('description')}}" name="description" autofocus required>
+                            </div>
+                         <div class="col-md-4"></div>
+                        </div>
+       </div>
+
+
+       <div class="row">
+                     <div class="form-group">
+                     <label for="standarPrice" class="col-md-3 control-label">standar Price</label>
+                     <div class="col-md-4">
+                       <input type="text"  placeholder="standar Price" class="form-control" value="{{old('standarPrice')}}" name="standarPrice" autofocus required>
+                    </div>
+                       <div class="col-md-4"></div>
+                    </div>
+        </div>
+
+
+       <div class="row">
+
+                              <div class="form-group">
+                                 <label for="weightNet" class="col-md-3 control-label">weight Net</label>
+                                  <div class="col-md-4">
+                                      <input type="text"  placeholder="weight Net" class="form-control" value="{{old('weightNet')}}" name="weightNet" autofocus required>
+                                  </div>
+                               <div class="col-md-4"></div>
+                              </div>
+                    </div>
+
+       <div class="row">
+
+                              <div class="form-group">
+                                 <label for="type" class="col-md-3 control-label">type</label>
+                                  <div class="col-md-4">
+                                      <input type="text"  placeholder="type" class="form-control" value="{{old('type')}}" name="type" autofocus required>
+                                  </div>
+                               <div class="col-md-4"></div>
+                              </div>
+                    </div>
+
+       <div class="row">
+
+                              <div class="form-group">
+                                 <label for="ref" class="col-md-3 control-label">uom Id</label>
+                                  <div class="col-md-4">
+                                      <input type="text"  placeholder="uom Id" class="form-control" value="{{old('uomId')}}" name="uomId" autofocus required>
+                                  </div>
+                               <div class="col-md-4"></div>
+                              </div>
+                    </div>
+
+       <div class="row">
+
+                              <div class="form-group ">
+                                 <label for="state" class="col-md-3 control-label">state</label>
+                                  <div class="col-md-4">
+                                      <input type="text"  placeholder="state" class="form-control" value="{{old('state')}}" name="state" autofocus required>
+                                  </div>
+                               <div class="col-md-4"></div>
+                              </div>
+         </div>
+
+
+                 <div class="form-group">
+                  <div class="col-md-3"></div>
+                     <div class="col-md-4">
+                         <button type="submit" class="btn btn-primary btn-block">Ajouter un article</button>
+                     </div>
+                 </div>
+
+             </form> <!-- /form -->
+
+
+
+
+         </div> <!-- ./container -->
+
+
+@endsection
