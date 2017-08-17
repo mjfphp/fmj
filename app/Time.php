109760f393be07440a0fb476306b7e831ed17360
mjf <mjf.php@gmail.com>
2017-08-17 09:52:10 +0100

    time & matiere

---
+++ app/Time.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Time extends Model
+{
+    protected $table="times";
+
+    public function opp()
+    {
+        return $this->belongsTo('App\Opp','opp_id');
+    }
+
+    public  function operator()
+    {
+        return $this->belongsTo('App\Operator','operator_id');
+    }
+}
---
+++ database/migrations/2017_08_17_080357_create_times_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTimesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('times', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('description');
+            $table->float('vh');
+            $table->integer('opp_id')->unsigned();
+            $table->integer('operator_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('times');
+    }
+}
